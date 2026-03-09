0a0bc882188db867ba5170efbb4c5fe87e3a65c9
AristideExe <aristidepr@gmail.com>
2026-03-10 00:50:33 +0100

    Ajoute le spawn des ennemis

--- Projet_DJV2/Assets/Scripts/Enemies/EnemyController.cs
+++ Projet_DJV2/Assets/Scripts/Enemies/EnemyController.cs
@@ -1,8 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+namespace Enemies
 {
-    
+    [RequireComponent(typeof(Mover))]
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private EnemyData enemyData;
+        
+        private Mover _mover;
+
+        private void Awake()
+        {
+            _mover =  GetComponent<Mover>();
+            _mover.SetSpeed(enemyData.speed);
+        }
+    }
 }
--- Projet_DJV2/Assets/Scripts/Level.cs
+++ Projet_DJV2/Assets/Scripts/Level.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Enemies;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class Level : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private Transform[] pathPoints;
+    
+    private EnemyController[] _enemies;
+
+    private void StartWave()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            var enemyPrefab = _enemies[Random.Range(0, _enemies.Length)];
+            var enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        var loadHandle = Addressables.LoadAssetsAsync<GameObject>("Enemy", null);
+        yield return loadHandle;
+        _enemies = loadHandle.Result.Select(go => go.GetComponent<EnemyController>()).ToArray();
+        StartWave();
+    }
 }

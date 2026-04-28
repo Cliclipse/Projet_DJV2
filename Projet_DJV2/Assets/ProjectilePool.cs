fb6d63a66092c3183f8a8fbf4ea69911d0657374
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2026-04-29 01:42:16 +0200

    Mise en place du préfab des pool d'un projectile

--- Projet_DJV2/Assets/ProjectilePool.cs
+++ Projet_DJV2/Assets/ProjectilePool.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Projectiles;
 using UnityEngine;
 
-public class ProjectilePool : MonoBehaviour
+public class MonoProjectilePool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField] private int projectilesPoolSize = 150;
+    [SerializeField] private Projectile projectilePrefabModel;
+    
+
+    private int _projectilesAvailable;
+
+    public bool isPoolReady; // Sera utile quand on fera une sorte de barre de chargement
+    
+    private Projectile[] _projectilesPool;
+
+    public Projectile GetAProjectile(Vector3 position, Quaternion rotation)
     {
-        
+        if (_projectilesAvailable > 0)
+        {
+            _projectilesAvailable--;
+            Projectile projectileFourni = _projectilesPool[_projectilesAvailable];
+            projectileFourni.transform.position = position;
+            projectileFourni.transform.rotation = rotation;
+            projectileFourni.gameObject.SetActive(true);
+            return projectileFourni;
+        }
+            Debug.Log("Pool de ce projectile vide, augmentation automatique de la pool pas encore implémentée");
+            return null;
+    }
+    public void PutBackAProjectile(Projectile projectile)
+    {
+        if (_projectilesAvailable > 0)
+        {
+            projectile.gameObject.SetActive(false);
+            _projectilesAvailable++;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void Start()
     {
-        
+        isPoolReady = false;
+        for (int i = 0; i < projectilesPoolSize; i++)
+        {
+            Instantiate(projectilePrefabModel);
+            projectilePrefabModel.gameObject.SetActive(false);
+            _projectilesPool[i] = projectilePrefabModel;
+
+        }
+        _projectilesAvailable = projectilesPoolSize;
+        isPoolReady = true;
     }
 }
